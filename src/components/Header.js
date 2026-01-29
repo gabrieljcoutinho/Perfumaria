d4609b98a78776080f6718b5bb482ae2a9170788
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-29 09:14:44 -0300

    ultima rota header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,6 +14,7 @@ const Header = () => {
             <li><Link to="/loja" className="ul-lista">Loja</Link></li>
             <li><Link to="/sobrenos" className="ul-lista">Sobre Nós</Link></li>
             <li><Link to="/servicos" className="ul-lista">Serviços</Link></li>
+            <li><Link to="/blog" className="ul-lista">Blog</Link></li>
       </ul>
       <img src={listaIconesHeader} alt="" />
     </header>
