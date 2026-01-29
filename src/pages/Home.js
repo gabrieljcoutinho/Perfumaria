20c285447425e44baf6195eceb6f90f2267a688a
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-29 09:52:13 -0300

    refaznedo imgs e rotas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import "../CSS/Home/home.css"
 
+import imgHome from "../assets/imgs/imgHomeInicial.png"
+
 const Home = () => {
   return (
     <div className='section'>
 
           <main className="conteudoInicial">
-            Conteudo Inicial
+                <h1>Elevate Your Spirit with <br/> Victory Scented Fragrances</h1>
+                <p>Shop now and embrace the sweet small of victory with Local Face</p>
+
+              <img src={imgHome} alt="" />
+
           </main>
 
 
