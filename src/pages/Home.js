c1fe58010bb162a80ed6ea48358e5f77afbab36c
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-29 10:57:36 -0300

    movimetnaodos paragrfos

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -88,7 +88,8 @@ const Home = () => {
           <h2 className='titutloNossoValores'>Our Values</h2>
 
 
-      <p className="nossoValoresParagrafo">
+   <div className="conteudoDosParagrafos">
+   <p className="nossoValoresParagrafo">
       At Local Face, our perfume retail store is built on a foundation of passion and authenticity. We believe in celebrating the individuality of every customer, providing a diverse collection of scents that resonate with their unique personality and style. Our dedicated team of fragrance enthusiasts is committed to creating a welcoming and inclusive environment, where connections are forged, and inspiration thrives.
 
       </p>
@@ -97,6 +98,7 @@ const Home = () => {
       <p className="nossoValoresParagrafo">
       Embracing sustainability and continuous learning, Local Face strives to be more than just a shopping destination; we are a community that inspires and empowers individuals on their fragrance journey.
         </p>
+   </div>
 
               </div>
 
