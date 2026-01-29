0c82659e5bcff63a569e8abaad23e8701a4d8e64
Gabriel <gabrieljcoutinho@gmail.com>
2026-01-29 09:13:30 -0300

    melhorais rotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import Home from '../pages/Home';
 import Loja from '../pages/Loja';
 import Sobrenos from '../pages/SobreNos';
+import Servicos from '../pages/Services';
 
 const AppRoutes = () => { // Mudamos o nome aqui
   return (
@@ -14,6 +15,7 @@ const AppRoutes = () => { // Mudamos o nome aqui
         <Route path="/" element={<Home />} />
         <Route path="/loja" element={<Loja />} />
         <Route path="/sobrenos" element={<Sobrenos />} />
+        <Route path="/servicos" element={<Servicos />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
