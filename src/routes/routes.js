466e79a36b2a3b643e0f059281a253599d5074ce
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-01-30 14:21:32 -0300

    tirand otrotas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../components/Header';
 import Home from '../pages/Home';
-import Loja from '../pages/Loja';
-import Sobrenos from '../pages/SobreNos';
-import Servicos from '../pages/Services';
-import Blog from '../pages/Blog';
-
 
 const AppRoutes = () => {
   return (
@@ -15,12 +10,6 @@ const AppRoutes = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-
-        {/* Descomente as linhas abaixo conforme for criando as páginas */}
-        <Route path="/loja" element={<Loja />} />
-        <Route path="/sobrenos" element={<Sobrenos />} />
-        <Route path="/servicos" element={<Servicos />} />
-        <Route path="/blog" element={<Blog />} />
 
         {/* Rota de segurança: se digitar algo errado, volta para a Home */}
         <Route path="*" element={<Navigate to="/" replace />} />
