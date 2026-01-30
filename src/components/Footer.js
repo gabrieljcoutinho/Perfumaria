5925006e910cac6bb680edd16ea7a5183d22823e
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-01-30 13:32:30 -0300

    teste

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,26 @@ import "../../src/CSS/Footer/index.css"
 const Footer = () => {
   return (
     <div className='Footer'>
+      <h4>Local Face</h4>
 
+      <div>
+        {/* Note a barra '/>' ao final e o 'className' */}
+        <input
+          type="email"
+          className="input"
+          id="Email"
+          name="Email"
+          placeholder="uiverse@verse.io"
+          autoComplete="off"
+        />
 
-<h4>Local Face</h4>
-
-
-        </div>
+        <input
+          className="button--submit"
+          value="Subscribe"
+          type="submit"
+        />
+      </div>
+    </div>
   )
 }
 
