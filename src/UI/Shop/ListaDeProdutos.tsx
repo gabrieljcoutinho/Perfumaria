589cd4c0f2f2ffcced99188637e9e37258f06f9c
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 11:28:10 -0300

    novas imgs

--- src/UI/Shop/ListaDeProdutos.tsx
+++ src/UI/Shop/ListaDeProdutos.tsx
@@ -1,12 +1,51 @@
 import React from 'react'
+
+import "../../CSS/Shop/listaProdutos.css"
+
 import img1 from "../../assets/imgShop/perfumeLoja1.png"
+import img2 from "../../assets/imgShop/perfumeLoja2.png"
+import img3 from "../../assets/imgShop/perfumeLoja3.png"
+import img4 from "../../assets/imgShop/perfumeLoja4.png"
+import img5 from "../../assets/imgShop/perfumeLoja5.png"
+import img6 from "../../assets/imgShop/perfumeLoja6.png"
+import img7 from "../../assets/imgShop/perfumeLoja7.png"
+import img8 from "../../assets/imgShop/perfumeLoja8.png"
+import img9 from "../../assets/imgShop/perfumeLoja9.png"
+import img10 from "../../assets/imgShop/perfumeLoja10.png"
+import img11 from "../../assets/imgShop/perfumeLoja11.png"
+import img12 from "../../assets/imgShop/perfumeLoja12.png"
+
+
 type Props = {}
 
 const ListaDeProdutos = (props: Props) => {
   return (
     <div>
 
+
 <img src={img1} alt="" />
+
+<img src={img2} alt="" />
+
+<img src={img3} alt="" />
+
+<img src={img4} alt="" />
+
+<img src={img5} alt="" />
+
+<img src={img6} alt="" />
+
+<img src={img7} alt="" />
+
+<img src={img8} alt="" />
+
+<img src={img9} alt="" />
+
+<img src={img10} alt="" />
+
+<img src={img11} alt="" />
+
+<img src={img12} alt="" />
 
     </div>
   )
