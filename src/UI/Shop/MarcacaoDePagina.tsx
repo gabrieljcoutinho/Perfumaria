5eadfd2b604e917844c1f703f0fac39d4b9e0bf2
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:53:26 -0300

    teste

--- src/UI/Shop/MarcacaoDePagina.tsx
+++ src/UI/Shop/MarcacaoDePagina.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import "../../CSS/Shop/marcacaoDePAgina.css"
+import React from 'react';
+import "../../CSS/Shop/marcacaoDePAgina.css";
 
 const MarcacaoDePagina = () => {
   return (
-    <div><h3 className='tituloShop'>Shop / Products</h3></div>
-  )
-}
+    <div>
+      <h3 className='tituloShop'>Shop / Products</h3>
+    </div>
+  );
+};
 
-export default MarcacaoDePagina
+export default MarcacaoDePagina;
