2de77e20f0821d17e8ac87f0d7be9a579420c182
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 10:59:05 -0300

    novo conteudo de tirulo para al oja

---
+++ src/UI/Shop/TituloPrincipalDaPaginaShop.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const TituloPrincipalDaPaginaShop = (props: Props) => {
+  return (
+    <div>
+        <h2>Best Selling Products</h2>
+    </div>
+  )
+}
+
+export default TituloPrincipalDaPaginaShop
