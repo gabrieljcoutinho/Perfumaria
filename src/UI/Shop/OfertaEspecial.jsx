dfb9cb3fe220afb423832d4eb6b0331e42fa4687
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 16:53:15 -0300

    conteudo

--- src/UI/Shop/OfertaEspecial.jsx
+++ src/UI/Shop/OfertaEspecial.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import "../../CSS/Shop/ofertaEspecial.css"
 
-
+import imgPromocaoPerfume from "../../assets/imgShop/ofertaEspecial.png"
 
 type Props = {}
 
 const OfertaEspecial = (props: Props) => {
   return (
     <div className='divOfertaEspecial'>
-        OfertaEspecia
+           <div className="conteudoEscrito">
+           Immerse yourself in the calming embrace of Aqua Serenity, a captivating fragrance that evokes the essence of water.
+           </div>
+
+           <div className="imgPromocao">
+            <img src={imgPromocaoPerfume} alt="" className='imgPromocaoLoja' />
+           </div>
         l</div>
   )
 }
