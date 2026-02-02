080321aadd9c2c1495193300f38301f45e86d915
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-02 17:00:05 -0300

    estilziação

--- src/UI/Shop/OfertaEspecial.jsx
+++ src/UI/Shop/OfertaEspecial.jsx
@@ -8,7 +8,7 @@ type Props = {}
 const OfertaEspecial = (props: Props) => {
   return (
     <div className='divOfertaEspecial'>
-        <h1>Especial Ofers</h1>
+        <h1 className='tituloSecaoPrincipal'>Especial Ofers</h1>
            <div className="conteudoEscrito">
             <h3>Limited Time Offer: 20% OFF on Aqua Serenity Perfume!</h3>
             <p className='paragrafoConteudoOferta'>Immerse yourself in the calming embrace of Aqua Serenity, a captivating fragrance that evokes the essence of water.</p>
