2056f5fd1bc5bccd462defddbdc3412273f632d2
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-06 17:10:06 -0300

    novo conteudo

---
+++ src/UI/Shop/PerfumeDeOuro.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../CSS/Shop/perfumeDeOuro.css"
+
+type Props = {}
+
+const PerfumeDeOuro = (props: Props) => {
+  return (
+    <div>PerfumeDdfgdgdsgdfeOuro</div>
+  )
+}
+
+export default PerfumeDeOuro
