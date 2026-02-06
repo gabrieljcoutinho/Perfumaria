d5271511b82a68ddf094713294265819de1de364
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-06 17:17:57 -0300

    img de ouro

--- src/UI/Shop/PerfumeDeOuro.jsx
+++ src/UI/Shop/PerfumeDeOuro.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import "../../CSS/Shop/perfumeDeOuro.css"
 
+import imgDeOuro from "../../assets/imgShop/perfumeDeOuro.png"
+
 type Props = {}
 
 const PerfumeDeOuro = (props: Props) => {
   return (
     <div className='mainPerfumeDeOuro'>
+            <img src={imgDeOuro} alt="" />
 
-      
     </div>
   )
 }
