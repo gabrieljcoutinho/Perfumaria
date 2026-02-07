7e4aa9f84242e303dc211ded48a2fd7ac2a6671f
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 08:56:36 -0300

    rota

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import MarcacaoDepagina from "../../src/UI/AboutUs/MarcacaoDePagina"
+import Aboutus from "../../src/UI/AboutUs/AboutUs.jsx"
 
 const AboutUs = () => {
   return (
