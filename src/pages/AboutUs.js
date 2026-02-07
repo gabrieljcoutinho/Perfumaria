9d30847f6ffd1bc121274f1e3e3c6e7fd0c948cf
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 10:07:41 -0300

    footer

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,6 +3,7 @@ import MarcacaoDepagina from "../../src/UI/AboutUs/MarcacaoDePagina"
 import Aboutus from "../../src/UI/AboutUs/AboutUs.jsx"
 import OurStory from "../../src/UI/AboutUs/OurStory.jsx"
 import AmostraDeBebida from "../../src/UI/AboutUs/AmostraDeBebida.jsx"
+import  Footer from "../components/Footer.js"
 
 const AboutUs = () => {
   return (
@@ -11,6 +12,7 @@ const AboutUs = () => {
         <Aboutus />
         <OurStory />
         <AmostraDeBebida />
+        <Footer />
     </div>
   )
 }
