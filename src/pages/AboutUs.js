498d2ce2c62b3796d276bc67cfca8abd568f38d3
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 09:55:38 -0300

    novo componete

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,6 +2,7 @@ import React from 'react'
 import MarcacaoDepagina from "../../src/UI/AboutUs/MarcacaoDePagina"
 import Aboutus from "../../src/UI/AboutUs/AboutUs.jsx"
 import OurStory from "../../src/UI/AboutUs/OurStory.jsx"
+import OuAmostraDeBebidarStory from "../../src/UI/AboutUs/AmostraDeBebida.jsx"
 
 const AboutUs = () => {
   return (
