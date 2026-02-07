92f8d051ff9f5f73dcbaf42c7737b83946531094
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-07 07:53:01 -0300

    trocando rodem

--- src/UI/Shop/PerfumeDeOuro.jsx
+++ src/UI/Shop/PerfumeDeOuro.jsx
@@ -11,13 +11,13 @@ const PerfumeDeOuro = (props: Props) => {
 
 
           <main className="conteudoDivPerfumeDeOuro">
-          <img src={imgDeOuro} alt=""  className='imgPerfumeDeOuro'/>
               <div className="textoConteudoPerfumeDeouro">
                   <h1>Limited Time Offer: 25% OFF <br/> on Golden Algel Perfume</h1>
                   <h2 >Golden Algel</h2>
                   <p className="paragrafoDeOuro">Unleash Your Divine Glow</p>
                   <p className='paragrafoDescricao'>Indulge in the divine allure of Golden Angel, a fragrance that embodies celestial elegance and radiance.</p>
               </div>
+              <img src={imgDeOuro} alt=""  className='imgPerfumeDeOuro'/>
           </main>
 
 
