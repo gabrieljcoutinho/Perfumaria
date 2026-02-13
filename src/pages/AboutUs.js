417e21bfe7e768fefe75ef477aa211b78189d160
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-13 11:14:14 -0300

    novo componete About

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,6 +3,7 @@ import MarcacaoDepagina from "../../src/UI/AboutUs/MarcacaoDePagina"
 import Aboutus from "../../src/UI/AboutUs/AboutUs.jsx"
 import OurStory from "../../src/UI/AboutUs/OurStory.jsx"
 import AmostraDeBebida from "../../src/UI/AboutUs/AmostraDeBebida.jsx"
+import OqueNosFazUnicos from "../../src/UI/AboutUs/OqueNosFazUnicos.jsx"
 import  Footer from "../components/Footer.js"
 
 const AboutUs = () => {
@@ -12,6 +13,7 @@ const AboutUs = () => {
         <Aboutus />
         <OurStory />
         <AmostraDeBebida />
+    <OqueNosFazUnicos/>
         <Footer />
     </div>
   )
