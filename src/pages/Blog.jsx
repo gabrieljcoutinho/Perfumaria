613ef2d6783aa5d213e75441322088bc764e9a6e
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:00:09 -0300

    renoemadno

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+import MarcacaoDePagina from '../UI/Blog/MarcacaoDePagina'
+
+
+
+const Blog = () => {
+  return (
+    <div>
+
+            <MarcacaoDePagina />
+
+    </div>
+  )
+}
+
+export default Blog
