1aa967b1e01f3d8b2f2f5622bd88f2ebda150243
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:10:56 -0300

    css novo

---
+++ src/UI/UI/Blog/Titulo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import  "../../../CSS/Blog/titulo.css"
+
+const Titulo = () => {
+  return (
+    <div>
+      <h3>wedjhalksjdhkjl</h3>
+    </div>
+  )
+}
+
+export default Titulo
