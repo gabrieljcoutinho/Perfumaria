58d0854275a77ac5c74ff5ebfaee452c652cfbe6
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 16:31:16 -0300

    estrelas

--- src/UI/Shop/ListaDeProdutos.jsx
+++ src/UI/Shop/ListaDeProdutos.jsx
@@ -205,6 +205,17 @@ const ListaDeProdutos = () => {
         <div className="divConteudo">
           <img src={img11} alt="Perfume 11" className='img'/>
           <h3 className='tituloDosPerfumesLista'>Glamourus Gilt</h3>
+
+
+          <ul className="estrela">
+        <li className="listaEstrelas"><img src={star} alt="" className='imgEstrelaListaClassificacao' /></li>
+        <li className="listaEstrelas"><img src={star} alt="" className='imgEstrelaListaClassificacao' /></li>
+        <li className="listaEstrelas"><img src={star} alt="" className='imgEstrelaListaClassificacao' /></li>
+        <li className="listaEstrelas"><img src={star} alt="" className='imgEstrelaListaClassificacao' /></li>
+        <li className="listaEstrelas"><img src={star} alt="" className='imgEstrelaListaClassificacao' /></li>
+        <li className="listaEstrelas"><img src={star} alt="" className='imgEstrelaListaClassificacao' /></li>
+      </ul>
+
           <p className="preco">R$: 160,00</p>
         </div>
 
