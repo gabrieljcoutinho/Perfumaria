1a304e4041148d8b9ed3f3e765c694d1e26d62a5
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 10:44:24 -0300

    renomeando

---
+++ src/UI/Blog/MarcacaoDePagina.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const MarcacaoDePagina = (props: Props) => {
+  return (
+    <div>
+
+        <h1 className='marcacaoDePaginaBlog'>Home / Blog / The Art of Curating a Luxury Perfume Collections A Symphony os Scenes and Stories</h1>
+
+    </div>
+  )
+}
+
+export default MarcacaoDePagina
