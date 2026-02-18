6053d7a7bfad3963b0cf3d22b46c89347dababfd
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:17:54 -0300

    TituloPrincipal

--- src/UI/Blog/ConteudoTodoPrincipal.jsx
+++ src/UI/Blog/ConteudoTodoPrincipal.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Titulo from '../UI/Blog/Titulo.jsx'
+import TituloPrincipal from '../UI/Blog/TituTituloPrincipallo.jsx'
 
 const ConteudoTodoPrincipal = () => {
   return (
     <div>
         <br /><br /><br /><br />
       <Titulo />
+      <TituloPrincipal />
     </div>
   )
 }
