e8a88866384c275a23ef0debaccfe19250ebb364
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:25:27 -0300

    nova import

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 
 import MarcacaoDePagina from '../UI/Blog/MarcacaoDePagina.jsx'
 import ConteudoTodoPrincipal from '../UI/Blog/ConteudoTodoPrincipal.jsx'
+import ConteudoImgPrincipalBlog from '../UI/Blog/ConteudoImgPrincipalBlog.jsx'
 
 
 
@@ -13,7 +14,7 @@ const Blog = () => {
 
             <MarcacaoDePagina />
             <ConteudoTodoPrincipal />
-
+          <ConteudoImgPrincipalBlog />
     </div>
   )
 }
