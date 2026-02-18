44a5bee3542f41478de6c4597760e01f0e10ee6b
Gabriel Coutinho <gabrieljcoutinho@gmail.com>
2026-02-18 11:05:09 -0300

    renomeando

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // Removida a extensão .tsx da importação
 import MarcacaoDePagina from '../UI/Shop/MarcacaoDePagina.jsx';
 import TituloPrincipalDaPaginaShop from "../UI/Shop/TituloPrincipalDaPaginaShop.jsx"
-import ListaProdutos from "../UI/Shop/ListaDeProdutos"
+import ListaProdutos from "../UI/Shop/ListaDeProdutos.jsx"
 import OfertaEspecial from "../UI/Shop/OfertaEspecial"
 import PerfumeDeOuro from "../UI/Shop/PerfumeDeOuro"
 import Footer from "../components/Footer"
